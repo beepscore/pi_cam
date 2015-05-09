4a089929ef264ac82457b1cf512b69badb392053
beepscore <support@beepscore.com>
2015-05-08 23:31:25 -0700

    In pi_cam.py list camera properties and default values. Flip horizontal, flip vertical.
    https://www.raspberrypi.org/documentation/usage/camera/python/README.md

--- pi_cam.py
+++ pi_cam.py
@@ -4,7 +4,26 @@ import picamera
 
 camera = picamera.PiCamera()
 
+# camera properties default values
+# camera.sharpness = 0
+# camera.contrast = 0
+# camera.brightness = 50
+# camera.saturation = 0
+# camera.ISO = 0
+# camera.video_stabilization = False
+# camera.exposure_compensation = 0
+# camera.exposure_mode = 'auto'
+# camera.meter_mode = 'average'
+# camera.awb_mode = 'auto'
+# camera.image_effect = 'none'
+# camera.color_effects = None
+# camera.rotation = 0
+# camera.hflip = False
+camera.hflip = True
+# camera.vflip = False
+camera.vflip = True
+# camera.crop = (0.0, 0.0, 1.0, 1.0)
+
 # take a picture
 image_name = 'image.jpg'
 camera.capture(image_name)
-
